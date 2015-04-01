041f897e25e63ee6adacc925b1e1b5e69f79d929
Tyler-Hilbert <tylerjoshuahilbert@gmail.com>
2015-04-01 16:50:49 -0700

    Added playing songs

---
+++ src/musicmaniac/Song.java
@@ -0,0 +1,13 @@
+package musicmaniac;
+
+public class Song {
+    private String path;
+    
+    public Song(String path) {
+        this.path = path;
+    }
+    
+    public String getPath() {
+        return path;
+    }
+}
--- src/musicmaniac/SongsView.java
+++ src/musicmaniac/SongsView.java
@@ -1,5 +1,8 @@
 package musicmaniac;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
@@ -10,14 +13,13 @@ import javafx.scene.control.ScrollPane;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 
-/**
- *
- * @author Tyler
- */
+
 public class SongsView {
     
-    ArrayList<Label> songs = new ArrayList<Label>();
+    MediaPlayer mediaPlayer;
     
     public SongsView(Stage primaryStage) {
         // Set up grid
@@ -27,21 +29,7 @@ public class SongsView {
         grid.setVgap(10);
         grid.setPadding(new Insets(25, 25, 25, 25));
         
-        // Add labels and action listeners
-        for (int i = 0; i < 100; i++) {
-            Label song = new Label("Song number " + i);
-
-            song.setOnMousePressed(new EventHandler<MouseEvent>() {
-                @Override
-                public void handle(MouseEvent me) {
-                    song.setText("Clicked");
-                }
-            });
-            
-            grid.add(song, 0, i);
-            songs.add(song);
-        }
-        
+        loadSongs(grid);
         
         ScrollPane sp = new ScrollPane();
         sp.setContent(grid);
@@ -50,5 +38,47 @@ public class SongsView {
         primaryStage.setTitle("Music Maniac");
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+    
+    private void loadSongs(GridPane grid) {
+        ArrayList<Song> songs = new ArrayList<Song>();
+        
+        try {
+            Files.walk(Paths.get("C:\\Users\\Tyler\\Music\\df")).forEach(filePath -> {
+                if (Files.isRegularFile(filePath)) {
+                    songs.add(new Song(filePath.toString()));
+                }
+            });
+        } catch(Exception ex) {
+            ex.printStackTrace();
+        }
+        
+        
+        
+        for (int i = 0; i < songs.size(); i++) {
+            Song song = new Song(songs.get(i).getPath());
+            
+            Label songLabel = new Label(song.getPath());
+
+            songLabel.setOnMousePressed(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent me) {
+                    playSong(song.getPath());
+                }
+            });
+            
+            grid.add(songLabel, 0, i);
+        }
+    }
+    
+    private void playSong(String path) {
+        // stop current player:
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+        }
+        
+        Media media = new Media(new File(path).toURI().toString());
+        mediaPlayer = new MediaPlayer(media);
+        mediaPlayer.play();
     }
 }
